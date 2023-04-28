96399173c8217148d1e6aa00780b2f87a95cb8b2
wdrayton <wdrayton@live.unc.edu>
2023-04-28 11:15:55 -0400

    Second submission

--- server.js
+++ server.js
@@ -1,13 +1,64 @@
 import minimist from 'minimist';
-import 'rps-cli';
-import 'rpsls-cli';
-import 'fs';
-import 'http';
-import minimist from 'minimist';
+import {playRPS} from "./lib/rpsls.js";
+import {playRPSLS} from "./lib/rpsls.js";
+import express from 'express';
+// import 'fs';
+// import 'http';
 
 const args = minimist(process.argv.slice(2));
 
+const app = express();
+
 const port = args.port || process.env.port || 5000;
 
-const shot = String(args.data);
+if (args.data != null) {
+    const shot = args.data;
+} else {
+    const shot = null;
+}
+
+app.get('/app/', (req, res) => {
+    res.statusCode = 200;
+    res.send("200 OK");
+    res.end();
+});
+
+app.get('/app/rps/', (req, res) => {
+    res.statusCode = 200;
+    res.send(playRPS(shot));
+    res.end();
+});
+
+app.get('/app/rps/play/', (req, res) => {
+    res.statusCode = 200;
+    res.send(playRPS(shot));
+    res.end();
+});
+
+app.get('/app/rps/play/' + ('rock/'|'paper/'|'scissors/'), (req, res) => {
+    res.statusCode = 200;
+    res.send(playRPS(shot));
+    res.end();
+});
+
+app.get('/app/rpsls/', (req, res) => {
+    res.statusCode = 200;
+    res.send(playRPSLS(shot));
+    res.end();
+})
+
+app.get('/app/rpsls/play/', (req, res) => {
+    res.statusCode = 200;
+    res.send(playRPSLS(shot));
+    res.end();
+});
+
+app.get('/app/rpsls/play/' + ('rock/'|'paper/'|'scissors/'|'lizard/'|'spock/'), (req, res) => {
+    res.statusCode = 200;
+    res.send(playRPSLS(shot));
+    res.end();
+});
+
+res.send("404 NOT FOUND");
+
 
